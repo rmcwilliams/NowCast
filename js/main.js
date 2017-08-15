85906b3704fd4c145d5688974c20d63a541e7241
McWilliams <rmcwilliams@usgs.gov>
2017-08-15 11:20:24 -0400

    Took out nested getAllUrlParams function from the document.ready function in main.js and main2.js

--- js/main.js
+++ js/main.js
@@ -33,68 +33,6 @@ $(document).ready(function () {
 	map.addLayer(markers);
 	var params = {};
 	getAllUrlParams();
-	function getAllUrlParams() {
-
-		// get query string from url (optional) or window
-		var queryString = window.location.search.slice(1);
-
-		// we'll store the parameters here
-		var obj = {};
-
-		// if query string exists
-		if (queryString) {
-
-			// stuff after # is not part of query string, so get rid of it
-			queryString = queryString.split('#')[0];
-
-			// split our query string into its component parts
-			var arr = queryString.split('&');
-
-			for (var i = 0; i < arr.length; i++) {
-				// separate the keys and the values
-				var a = arr[i].split('=');
-
-				// in case params look like: list[]=thing1&list[]=thing2
-				var paramNum = undefined;
-				var paramName = a[0].replace(/\[\d*\]/, function (v) {
-					paramNum = v.slice(1, -1);
-					return '';
-				});
-
-				// set parameter value (use 'true' if empty)
-				var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
-
-				// (optional) keep case consistent
-				paramName = paramName.toLowerCase();
-				paramValue = paramValue.toLowerCase();
-
-				// if parameter name already exists
-				if (obj[paramName]) {
-					// convert value to array (if still string)
-					if (typeof obj[paramName] === 'string') {
-						obj[paramName] = [obj[paramName]];
-					}
-					// if no array index number specified...
-					if (typeof paramNum === 'undefined') {
-						// put the value on the end of the array
-						obj[paramName].push(paramValue);
-					}
-					// if array index number specified...
-					else {
-						// put the value at that index number
-						obj[paramName][paramNum] = paramValue;
-					}
-				}
-				// if param name doesn't exist yet, set it
-				else {
-					obj[paramName] = paramValue;
-				}
-			}
-		}
-		URLparams = obj;
-		// return obj;
-	}
-
 	/*if ("predState" in getAllUrlParams()) {
 		
 		theChosenState = getAllUrlParams().predState.toUpperCase();
@@ -229,6 +167,68 @@ $(document).ready(function () {
 	});*/
 	//end document ready function
 });
+
+function getAllUrlParams() {
+
+	// get query string from url (optional) or window
+	var queryString = window.location.search.slice(1);
+
+	// we'll store the parameters here
+	var obj = {};
+
+	// if query string exists
+	if (queryString) {
+
+		// stuff after # is not part of query string, so get rid of it
+		queryString = queryString.split('#')[0];
+
+		// split our query string into its component parts
+		var arr = queryString.split('&');
+
+		for (var i = 0; i < arr.length; i++) {
+			// separate the keys and the values
+			var a = arr[i].split('=');
+
+			// in case params look like: list[]=thing1&list[]=thing2
+			var paramNum = undefined;
+			var paramName = a[0].replace(/\[\d*\]/, function (v) {
+				paramNum = v.slice(1, -1);
+				return '';
+			});
+
+			// set parameter value (use 'true' if empty)
+			var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+			// (optional) keep case consistent
+			paramName = paramName.toLowerCase();
+			paramValue = paramValue.toLowerCase();
+
+			// if parameter name already exists
+			if (obj[paramName]) {
+				// convert value to array (if still string)
+				if (typeof obj[paramName] === 'string') {
+					obj[paramName] = [obj[paramName]];
+				}
+				// if no array index number specified...
+				if (typeof paramNum === 'undefined') {
+					// put the value on the end of the array
+					obj[paramName].push(paramValue);
+				}
+				// if array index number specified...
+				else {
+					// put the value at that index number
+					obj[paramName][paramNum] = paramValue;
+				}
+			}
+			// if param name doesn't exist yet, set it
+			else {
+				obj[paramName] = paramValue;
+			}
+		}
+	}
+	URLparams = obj;
+	// return obj;
+}
 
 function on() {
 	document.getElementById("overlay").style.display = "block";
